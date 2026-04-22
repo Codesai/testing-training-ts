45b9f9d4205528332b6808b63457f73ad3975dff
trikitrok <trikitrok@gmail.com>
2026-04-22 18:12:26 +0200

    Configure type-checking for Jest tests and update related dependencies

--- birthday-greetings/jest.config.js
+++ birthday-greetings/jest.config.js
@@ -1,7 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageDirectory: "reports/coverage",
-  coverageReporters: ["lcov", "text"],
+    coverageDirectory: "reports/coverage",
+    coverageReporters: ["lcov", "text"],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    testPathIgnorePatterns: [
+        "<rootDir>/build/",
+        "<rootDir>/node_modules/",
+        "<rootDir>/.stryker-tmp"
+    ]
 };
